6d07bda798bf64fcd42598d803ac0c3973638b0e
Miselaytes Anton <miselaytes.anton@ya.ru>
2014-05-05 21:53:40 +0200

    init

---
+++ server.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var app = express();
+var server = app.listen(process.env.PORT || 5000);
+var io = require('socket.io').listen(server);
+app.use(express.static(__dirname + '/public'));
+
+
+io.sockets.on('connection', function (socket){
+
+  // convenience function to log server messages on the client
+	function log(){
+		var array = [">>> Message from server: "];
+	  for (var i = 0; i < arguments.length; i++) {
+	  	array.push(arguments[i]);
+	  }
+	    socket.emit('log', array);
+	}
+
+	socket.on('message', function (message) {
+		log('Got message:', message);
+    // for a real app, would be room only (not broadcast)
+		socket.broadcast.emit('message', message);
+	});
+
+	socket.on('create or join', function (room) {
+		var numClients = io.sockets.clients(room).length;
+
+		log('Room ' + room + ' has ' + numClients + ' client(s)');
+		log('Request to create or join room ' + room);
+
+		if (numClients === 0){
+			socket.join(room);
+			socket.emit('created', room);
+		} else if (numClients === 1) {
+			io.sockets.in(room).emit('join', room);
+			socket.join(room);
+			socket.emit('joined', room);
+		} else { // max two clients
+			socket.emit('full', room);
+		}
+		socket.emit('emit(): client ' + socket.id + ' joined room ' + room);
+		socket.broadcast.emit('broadcast(): client ' + socket.id + ' joined room ' + room);
+
+	});
+
+});
