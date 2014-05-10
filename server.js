655434b9779246878a90c4922a08bfdc76b50980
Miselaytes Anton <miselaytes.anton@ya.ru>
2014-05-10 17:54:58 +0200

    working tutorial video chat(without audio)

--- server.js
+++ server.js
@@ -8,24 +8,6 @@ var index = require('./routes/index');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-//router
-/*
-app.get('/:roomName', function(req, res){
-  app.locals.roomName = req.params.roomName;
-});
-
-
-router.use(function(req, res, next) {
-	app.locals.roomName = req.params.roomName;
-	//res.send('bar');
-	next();
-});
-router.use(express.static(__dirname + '/public'));
-
-app.use('/bar', router);
-*/
-
 app.use(express.static(__dirname + '/public'));
 
 //routes
@@ -33,7 +15,6 @@ app.get('/', index);
 app.get('/:roomName', index);
 
 io.sockets.on('connection', function (socket){
-  // convenience function to log server messages on the client
 	function log(){
 		var array = [">>> Message from server: "];
 	  for (var i = 0; i < arguments.length; i++) {
@@ -43,8 +24,8 @@ io.sockets.on('connection', function (socket){
 	}
 
 	socket.on('message', function (message) {
-		log('Got message:', message);
-    // for a real app, would be room only (not broadcast)
+		log('Got message: ', message);
+    // For a real app, should be room only (not broadcast)
 		socket.broadcast.emit('message', message);
 	});
 
@@ -52,12 +33,12 @@ io.sockets.on('connection', function (socket){
 		var numClients = io.sockets.clients(room).length;
 
 		log('Room ' + room + ' has ' + numClients + ' client(s)');
-		log('Request to create or join room ' + room);
+		log('Request to create or join room', room);
 
-		if (numClients === 0){
+		if (numClients == 0){
 			socket.join(room);
 			socket.emit('created', room);
-		} else if (numClients === 1) {
+		} else if (numClients == 1) {
 			io.sockets.in(room).emit('join', room);
 			socket.join(room);
 			socket.emit('joined', room);
