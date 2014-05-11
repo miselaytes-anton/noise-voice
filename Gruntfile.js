86599606cdbd7262d5d27b48ff3fe314b38f813e
Miselaytes Anton <miselaytes.anton@ya.ru>
2014-05-11 18:37:12 +0200

    load js scripts depending on url/path

---
+++ Gruntfile.js
@@ -0,0 +1,56 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+	concat: {
+		options: {
+		  separator: ';',
+		},
+		dist: {
+		  src: [ "public/_/js/*.js" ],
+		  dest: "public/js/main.js"
+		}
+	},
+	uglify: {
+		production: {
+		  files: [
+                {
+                    src: ["public/js/main.js"],
+                    dest: ["public/js/main.min.js"]
+                }
+			]
+		  
+		}
+	  },
+	less: {
+		development: {
+			files: {
+			  "public/css/style.css": "public/_/less/style.less"
+			}
+		}
+	},
+	watch :{
+		development: {
+			less:{
+				files: ['public/_/less/*.less'],
+				tasks: ['less']
+			},
+			js: {
+				files: [ 'public/_/js/*.js' ],
+				tasks: ['uglify']
+			}
+		}
+	}
+  });
+
+  // Load the plugin that provides the "uglify" task.
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+  // Default task(s).
+	grunt.registerTask('default', ['less']);
+	//grunt.registerTask('less', ['less']);
+	
+};
--- routes/templates.js
+++ routes/templates.js
@@ -1,7 +1,9 @@
+var url = require('url');
 exports.index = function(req, res){
     res.render('index');
 };
 exports.room = function(req, res){
+	res.locals.path = req.path;
     res.render('room', {roomName: req.params.roomName,  domain: req.get('host')});
 };
 
